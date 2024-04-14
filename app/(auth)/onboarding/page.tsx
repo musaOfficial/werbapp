51aa427840905395d6944dadcc278c3dbc69a9b4
Musa Arslan <officialmusa58@gmail.com>
2024-04-14 10:08:55 +0200

    message

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,18 +1,28 @@
 "use client"
 import { currentUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
+import { useUser } from "@clerk/nextjs";
+import { Clerk } from "@clerk/nextjs/server";
 export default async function Page(){
-
-    const user = currentUser();
+    const { isLoaded, isSignedIn, user } = useUser();
     const router = useRouter();
 
-    async function handleSubmit(){
-        const response = await fetch("https://werbapp.vercel.app/api/webhooks", {
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        const response = await fetch("https://werbapp.vercel.app/api/users", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+               "Content-Type": "application/json" 
             },
-        })
+            body: JSON.stringify({
+                clerkId: user?.id,
+                email: user?.emailAddresses[0].emailAddress,
+                password: "aösdklfjaslkdf",
+                firstName: user?.firstName,
+                lastName: user?.lastName,
+                coins: 10,
+            })
+        }).then(res => res.json());
 
         if(response.ok){
             router.replace("/");

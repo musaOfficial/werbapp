6f09c4fa48222aed4bd00f47e048abcd4c17d670
Musa Arslan <officialmusa58@gmail.com>
2024-04-14 09:43:17 +0200

    Webhook activated 4

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -6,7 +6,24 @@ export default async function Page(){
     const user = currentUser();
     const router = useRouter();
 
+    async function handleSubmit(){
+        const response = await fetch("https://werbapp.vercel.app/api/webhooks", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+
+        if(response.ok){
+            router.replace("/");
+        } else {
+            console.log("Something went wrong")
+        }
+    }
+
     return (
-        <div>Onboarding...</div>
+        <form onSubmit={handleSubmit}>
+            <button type="submit">Weiter zum Homepage</button>
+        </form>
     )
 }
--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -3,7 +3,6 @@ import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
 import { createOrUpdateUser } from '@/actions/user'
 export async function POST(req: Request) {
- 
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
  
